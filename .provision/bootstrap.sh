66f0151f9e1e77bb6bd4e2e081ed1ace7d4206bd
ktsuench <will289@hotmail.com>
2017-11-12 04:00:29 -0500

    added in ssl cert setup

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -18,6 +18,11 @@ sudo npm install -g pm2
 # automate PM2 service to start at startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 
+## --- Install Certbot --- ##
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y python-certbot-nginx
+
 ## --- Install NGINX web server --- ##
 sudo apt-get install -y nginx
 # copy webserver file over
@@ -26,7 +31,13 @@ sudo cp /vagrant/.provision/default /etc/nginx/sites-available
 sudo systemctl restart nginx
 # allow NGINX through firewall
 sudo ufw allow 'Nginx Full'
+sudo ufw enable
 
 ## --- Install Sympm to link node modules properly --- ##
 ## uncomment this section only if you cannot run this script in admin/superuser mode ##
 ## sudo npm install -g sympm
+
+## --- Generate SSL certificate --- ##
+## refer to https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-16-04#step-2-—-setting-up-nginx
+# sudo certbot --nginx -d ruhacks.com -d www.ruhacks.com
+# sudo certbot renew --dry-run
