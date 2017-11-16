41430f226fa91df96a4d628eb97210e471f8dcb5
ktsuench <will289@hotmail.com>
2017-11-16 01:38:26 -0500

    removed www.ruhacks.com sub domain

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -39,5 +39,5 @@ sudo ufw enable
 
 ## --- Generate SSL certificate --- ##
 ## refer to https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-16-04#step-2-—-setting-up-nginx
-# sudo certbot --nginx -d ruhacks.com -d www.ruhacks.com
+# sudo certbot --nginx -d ruhacks.com
 # sudo certbot renew --dry-run
