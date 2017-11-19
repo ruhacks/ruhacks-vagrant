ca3ab182b1d234ab036772bcea2d50833510d8d4
ktsuench <will289@hotmail.com>
2017-11-18 23:38:54 -0500

    moved nodejs and pm2 install to be after nginx

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -8,16 +8,6 @@ sudo apt-get update
 cd ~
 sudo apt-get install build-essential -y
 
-## --- Install NodeJS v6.x --- ###
-sudo curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install -y nodejs
-
-## --- Install PM2 Process Manager --- ###
-sudo npm install -g pm2
-# automate PM2 service to start at startup
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
 ## --- Install Certbot --- ##
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
@@ -32,6 +22,16 @@ sudo systemctl restart nginx
 # allow NGINX through firewall
 sudo ufw allow 'Nginx Full'
 sudo ufw enable
+
+## --- Install NodeJS v6.x --- ###
+sudo curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+
+## --- Install PM2 Process Manager --- ###
+sudo npm install -g pm2
+# automate PM2 service to start at startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 
 ## --- Install Sympm to link node modules properly --- ##
 ## uncomment this section only if you cannot run this script in admin/superuser mode ##
