5e721db202dc494a8498a216b95126001d423d27
ktsuench <will289@hotmail.com>
2017-11-27 02:56:48 -0500

    added in 2018.ruhacks.com domain

--- setup-servers.sh
+++ setup-servers.sh
@@ -2,7 +2,7 @@
 
 ## --- Clone the repositories for server --- ##
 
-## --- 2017 --- ##
+## --- 2017 --- ##  
 # check that the 2017 dir doesn't exist
 if [ ! -d '2017' ]; then
   cd /vagrant
@@ -25,19 +25,23 @@ fi
 # check that the 2018 dir doesn't exist
 if [ ! -d '2018' ]; then
   cd /vagrant
+  mkdir 2018
 
   echo 'Grabbing 2018 repo from Github'
-  sudo git clone https://github.com/ruhacks/2018-website-placeholder 2018
+  #sudo git clone https://github.com/ruhacks/2018-website-placeholder 2018
+  sudo git clone https://github.com/ruhacks/ruhacks-2018-website-frontend 2018/frontend
 
-  echo 'Creating copy of .env.template and renaming to .env'
-  sudo cp /vagrant/2018/.env.template /vagrant/2018/.env
+  #echo 'Creating copy of .env.template and renaming to .env'
+  #sudo cp /vagrant/2018/.env.template /vagrant/2018/.env
 
-  if [ `command -v npm` != '' ]; then
-    cd /vagrant/2018
+  #if [ `command -v npm` != '' ]; then
+  #  cd /vagrant/2018
 
-    echo 'Installing node modules'
-    sudo npm install
-  fi
+  #  echo 'Installing node modules'
+  #  sudo npm install
+  #fi
+
+  echo '2018 frontend accessible now'
 fi
 
 if [ `command -v pm2` != '' ]; then
@@ -46,8 +50,8 @@ if [ `command -v pm2` != '' ]; then
   echo 'Starting up 2017 server using pm2'
   sudo pm2 start server.js --name 2017
 
-  cd /vagrant/2018
+  #cd /vagrant/2018
 
-  echo 'Starting up 2018 server using pm2'
-  sudo pm2 start server.js --name 2018
+  #echo 'Starting up 2018 server using pm2'
+  #sudo pm2 start server.js --name 2018
 fi
